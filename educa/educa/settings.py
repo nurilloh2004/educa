8e8a150963f668928e67764d1fe0511932965731
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2022-08-01 20:38:21 +0500

    all

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'postgres',
+        'PASSWORD': 'zxcvasdg',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
