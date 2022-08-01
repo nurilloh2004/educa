d6da5647108698ac2b18aeeb71316eea3dfcb38e
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2022-08-01 17:35:58 +0500

    all

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Subject, Course, Module
+from django.contrib import admin
+
+# use memcache admin index site
+admin.site.index_template = 'memcache_status/admin_index.html'
 
 
 @admin.register(Subject)
